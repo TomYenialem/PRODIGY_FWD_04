8325246eefeeb068b57b41e3ad8d5264ca189f30
TomYenialem <tom21g2008@gmail.com>
2025-03-18 08:46:37 +0300

    modify the path for index.html

--- backend/Index.js
+++ backend/Index.js
@@ -7,13 +7,14 @@ const loginRouter = require("./routes/login.routes");
 const messageRoute = require("./routes/message.routes");
 const cookieparser = require("cookie-parser");
 const getAllUsers = require("./routes/getUsers.routes");
-const logoutRouter = require("./routes/logout.routes")
-const {httpServer,app}=require('./Socket/Socket')
+const logoutRouter = require("./routes/logout.routes");
+const { httpServer, app } = require("./Socket/Socket");
 const cors = require("cors");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 
-
+// CORS Configuration
 app.use(
   cors({
     origin: [
@@ -25,14 +26,26 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieparser());
+
+// API Routes
 app.use(signinRouter);
 app.use(loginRouter);
 app.use(messageRoute);
 app.use(getAllUsers);
-app.use(logoutRouter)
+app.use(logoutRouter);
+
+// Serve React Frontend
+app.use(express.static(path.join(__dirname, "../frontend/build"))); // Adjust path if needed
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+// Start Server
 httpServer.listen(port, () => {
   mongoDb();
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on port ${port}`);
 });
